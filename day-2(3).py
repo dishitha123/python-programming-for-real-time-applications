1511da3bc6b6f62be8d4b52ed084228538de3bf8
dishitha123 <122262857+dishitha123@users.noreply.github.com>
2023-01-10 20:02:34 +0530

    Add files via upload

---
+++ day-2(3).py
@@ -0,0 +1,6 @@
+count0fWords = len("Hello This is python programming".split())
+print("Count of Words in the given Sentence:", count0fWords)
+
+print(len("Hello This is python programming".split()))
+
+print(len(input("Enter Input:").split()))
